a21fa1e146263aae8b4fb0f1cc01e1b3f24d61e2
Pickles3201 <pickles3201@gmail.com>
2018-05-09 00:28:09 -0500

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,20 @@
+import org.telegram.telegrambots.ApiContextInitializer;
+import org.telegram.telegrambots.TelegramBotsApi;
+import org.telegram.telegrambots.exceptions.TelegramApiException;
+
+public class Main
+{
+    public static void main(String[] args)
+    {
+        ApiContextInitializer.init();
+        TelegramBotsApi telegramBotsApi = new TelegramBotsApi();
+        try
+        {
+            telegramBotsApi.registerBot(new SnakerHomesBot());
+        }
+        catch(TelegramApiException e)
+        {
+            e.printStackTrace();
+        }
+    }
+}
